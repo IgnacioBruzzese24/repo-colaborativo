4bf6e7d65b6cc55a0ade541b05eafc85fab26de4
jkleinlarroude-tech <jkleinlarroude@udesa.edu.ar>
2026-03-20 16:27:49 -0300

    Update programa_principal.py

--- programa_principal.py
+++ programa_principal.py
@@ -1,7 +1,18 @@
 # -*- coding: utf-8 -*-
 """
-Created on Fri Mar 13 15:44:31 2026
+Created on Fri Mar 13 16:03:24 2026
 
 @author: nacho
 """
+#from funciones_habitos import registrar_habitos, analizar_habitos
+#from funciones_habitos import analizar_habitos
+import funciones_habitos
+
+
+lista = registrar_habitos()
+
+resultado = analizar_habitos(lista)
+
+print (' resumen de actividades')
+print(resultado)
 
