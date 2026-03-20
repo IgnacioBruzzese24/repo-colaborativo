72cd56a75d4094291518a83194c024d32211eedf
IgnacioBruzzese24 <161730915+IgnacioBruzzese24@users.noreply.github.com>
2026-03-20 16:32:10 -0300

    Commit

--- programa_principal.py
+++ programa_principal.py
@@ -4,9 +4,9 @@ Created on Fri Mar 13 16:03:24 2026
 
 @author: nacho
 """
-#from funciones_habitos import registrar_habitos, analizar_habitos
-#from funciones_habitos import analizar_habitos
-import funciones_habitos
+from funciones_habitos import registrar_habitos, analizar_habitos
+
+
 
 
 lista = registrar_habitos()
